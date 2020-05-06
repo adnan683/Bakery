bfe8b66385dade3609b1da5aeea151885b05c96a
Adnan Kicin <adnan.kicin@hauertmann.net>
2020-05-06 12:17:46 +0200

    Add project files.

---
+++ Bakery/Data/Migrations/20200506084927_CreateDatabase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bakery.Data.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ImageFileName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageFileName", "Name", "Price" },
+                values: new object[] { 1, "Beschte Döner wo gibt", "doener.jpg", "Döner Tasche", 8.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageFileName", "Name", "Price" },
+                values: new object[] { 2, "Ein kleiner Snack für zwischendurch", "burger.png", "Burger", 9.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageFileName", "Name", "Price" },
+                values: new object[] { 3, "Die schöne alternative zur Pizza", "pide.jpg", "Pide", 5.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageFileName", "Name", "Price" },
+                values: new object[] { 4, "Lamm auf die beste Art und Weise", "kuzu.jpg", "Kuzu Pirzola", 1.49m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageFileName", "Name", "Price" },
+                values: new object[] { 5, "Baklava geht immer", "baklava.jpg", "Baklava", 5.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageFileName", "Name", "Price" },
+                values: new object[] { 6, "Wer es bis jetzt noch nicht probiert, dann wirds mal Zeit", "kunefa.jpg", "Künefe", 8.99m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Bakery/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Bakery.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Bakery.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Döner Tasche",
+                    Description = "Beschte Döner wo gibt",
+                    Price = 8.99m,
+                    ImageName = "doener.jpg"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Burger",
+                    Description = "Ein kleiner Snack für zwischendurch",
+                    Price = 9.99m,
+                    ImageName = "burger.png"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Pide",
+                    Description = "Die schöne alternative zur Pizza",
+                    Price = 5.99m,
+                    ImageName = "pide.jpg"
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "Kuzu Pirzola",
+                    Description = "Lamm auf die beste Art und Weise",
+                    Price = 1.49m,
+                    ImageName = "kuzu.jpg"
+                },
+                new Product
+                {
+                    Id = 5,
+                    Name = "Baklava",
+                    Description = "Baklava geht immer",
+                    Price = 5.99m,
+                    ImageName = "baklava.jpg"
+                },
+                new Product
+                {
+                    Id = 6,
+                    Name = "Künefe",
+                    Description = "Wer es bis jetzt noch nicht probiert, dann wirds mal Zeit",
+                    Price = 8.99m,
+                    ImageName = "kunefa.jpg"
+                }
+            );
+            return modelBuilder;
+        }
+    }
+}
